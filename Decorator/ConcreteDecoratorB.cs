59ec3daff2ca65ae28c8db4ac534fc4713a473ac
shadowue@hotmail.com <shadowue@hotmail.com>
2020-07-13 14:10:00 +0200

    Decorator pattern added

---
+++ Decorator/ConcreteDecoratorB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Decorator
+{
+    internal class ConcreteDecoratorB : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            AddedBehavior();
+            Console.WriteLine("ConcreteDecoratorB.Operation()");
+        }
+
+        private void AddedBehavior()
+        {
+        }
+    }
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Decorator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+ 
+            // Create ConcreteComponent and two Decorators
+
+            ConcreteComponent c = new ConcreteComponent();
+            ConcreteDecoratorA d1 = new ConcreteDecoratorA();
+            ConcreteDecoratorB d2 = new ConcreteDecoratorB();
+
+            // Link decorators
+
+            d1.SetComponent(c);
+            d2.SetComponent(d1);
+
+            d2.Operation();
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+}
