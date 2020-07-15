4ec7fff8297d5da75c57fad8fffd1b071280b75b
shadowue@hotmail.com <shadowue@hotmail.com>
2020-07-15 14:43:31 +0200

    Added Facade docs and project

---
+++ Facade/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Facade facade = new Facade();
+
+            facade.MethodA();
+            facade.MethodB();
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Facade/SubSystemOne.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Facade
+{
+    internal class SubSystemOne
+    {
+        public void MethodOne()
+        {
+            Console.WriteLine(" SubSystemOne Method");
+        }
+    }
+}
