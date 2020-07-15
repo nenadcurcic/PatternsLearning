1030e5c44b9e5641e73bb111eaebb405c40ff062
shadowue@hotmail.com <shadowue@hotmail.com>
2020-07-15 15:51:26 +0200

    Added proxy pattern and docs

---
+++ Proxy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Proxy
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Create proxy and request a service
+
+            Proxy proxy = new Proxy();
+            proxy.Request();
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+}
