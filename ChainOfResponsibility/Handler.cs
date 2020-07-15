5738050e4ad44bd1aac233c60eaf94b010237a0b
shadowue@hotmail.com <shadowue@hotmail.com>
2020-07-16 00:15:02 +0200

    Added ChainOfResponsibility pattern, updated docs

---
+++ ChainOfResponsibility/Handler.cs
@@ -0,0 +1,14 @@
+﻿namespace ChainOfResponsibility
+{
+    public abstract class Handler
+    {
+        protected Handler successor;
+
+        public void SetSuccessor(Handler successor)
+        {
+            this.successor = successor;
+        }
+
+        public abstract void HandleRequest(int request);
+    }
+}
