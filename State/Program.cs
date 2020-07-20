0456dfa7ca7269fd0b32c13adb18d04550a27868
shadowue@hotmail.com <shadowue@hotmail.com>
2020-07-20 12:18:29 +0200

    Implemented State pattern, updated docs

--- State/Program.cs
+++ State/Program.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace State
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             // Setup context in a state
 
