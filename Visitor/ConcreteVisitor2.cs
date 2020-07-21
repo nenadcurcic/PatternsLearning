414b510b9f041ffb45db3e6ec9aa2aee69b14377
shadowue@hotmail.com <shadowue@hotmail.com>
2020-07-21 12:16:54 +0200

    Finished Visitor project, updated docs.

---
+++ Visitor/ConcreteVisitor2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Visitor
+{
+    internal class ConcreteVisitor2 : Visitor
+    {
+        public override void VisitConcreteElementA(
+      ConcreteElementA concreteElementA)
+        {
+            Console.WriteLine("{0} visited by {1}",
+              concreteElementA.GetType().Name, this.GetType().Name);
+        }
+
+        public override void VisitConcreteElementB(
+          ConcreteElementB concreteElementB)
+        {
+            Console.WriteLine("{0} visited by {1}",
+              concreteElementB.GetType().Name, this.GetType().Name);
+        }
+    }
+}
