6bc39c26b7b42ceb6191502f3bc74535ed9f7076
shadowue@hotmail.com <shadowue@hotmail.com>
2020-09-24 10:20:33 +0200

    Add real generics

---
+++ GenericsDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GenericsDemo
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            bool result = MyClass<int>.IsEqual(1, 1);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+
+    internal static class MyClass<T>
+    {
+        static internal bool IsEqual(double x, T y)
+        {
+            Console.WriteLine(y.GetType().ToString());
+            return x.Equals(y);
+        }
+    }
+}
